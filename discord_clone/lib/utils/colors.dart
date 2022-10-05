000588ce18f7a3c9b034480eb68d660446170259
Bùi Văn Toán <buivantoanbg2001@gmail.com>
2022-10-05 13:35:11 +0700

    modified structure ChatScreen

--- discord_clone/lib/utils/colors.dart
+++ discord_clone/lib/utils/colors.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
-const backgroundColor = Color.fromARGB(255, 48, 49, 54);
-const primaryTextColor = Colors.white;
-const secondaryTextColor = Color.fromARGB(255, 128, 129, 133);
+const whiteColor = Colors.white;
+const blackColor = Colors.black;
+
+// const backgroundColor = Color.fromARGB(255, 48, 49, 54);
+// const primaryTextColor = Colors.white;
+// const secondaryTextColor = Color.fromARGB(255, 128, 129, 133);
+
+const chatIconColor = Color(0XFFBABBC0);
+const chatHeaderColor = Color(0XFF28292E);
+const chatBodyColor = Color(0XFF303136);
+const chatBackgroundWidgetColor = Color(0XFF1D1E21);
+const chatTextSecondaryColor = Color(0xFF808185);
