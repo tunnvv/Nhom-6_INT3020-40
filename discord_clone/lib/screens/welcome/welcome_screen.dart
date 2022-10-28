95270942d6ad6e8887807798a96cd55fb7efbbba
Bùi Văn Toán <buivantoanbg2001@gmail.com>
2022-10-28 11:48:30 +0700

    Closes #19: add welcome_screen_background.jpg, add custom color, modified gap between elements

--- discord_clone/lib/screens/welcome/welcome_screen.dart
+++ discord_clone/lib/screens/welcome/welcome_screen.dart
@@ -9,100 +9,86 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimaryLightColor,
-      body: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Positioned(
-            top: 65.0,
-            left: 75.0,
-            right: 70.0,
-            child: Image.asset(
-              "assets/images/discord_logo.png",
-              fit: BoxFit.cover,
-            ),
-          ),
-          Positioned(
-            top: 155.0,
-            left: 55.0,
-            right: 50.0,
-            child: Image.asset(
-              "assets/images/discord_characters.png",
-              fit: BoxFit.cover,
-            ),
-          ),
-          const Positioned(
-              top: 455.0,
-              left: 35.0,
-              right: 30.0,
-              child: Text(
-                "Chào mừng bạn đến với Discord",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              )),
-          const Positioned(
-            top: 540.0,
-            left: 35.0,
-            right: 30.0,
-            child: Text(
-              "Tham gia cùng hơn 100 triệu người dùng Discord và trò chuyện với bạn bè và các cộng đồng khác nhau.",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
-            ),
-          ),
-          Positioned(
-            top: 640.0,
-            left: 20.0,
-            right: 16.0,
-            child: SizedBox(
-              height: 42.0,
-              width: 130,
-              child: TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(kSignupBntColor),
-                ),
-                child: const Text(
-                  "Đăng ký",
-                  style: TextStyle(
-                      color: kPrimaryLightColor,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold),
-                ),
+      body: Container(
+        color: welcomeBackgroundColor,
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Image.asset(
+                "assets/images/welcome_screen_background.jpg",
+                fit: BoxFit.contain,
               ),
             ),
-          ),
-          Positioned(
-            top: 690.0,
-            left: 20.0,
-            right: 16.0,
-            child: SizedBox(
-              height: 42.0,
-              width: 130,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SigninScreen()),
-                  );
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(kSigninBntColor),
-                ),
-                child: const Text(
-                  "Đăng nhập",
-                  style: TextStyle(
-                      color: kPrimaryLightColor,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold),
-                ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: const <Widget>[
+                  Text(
+                    "Chào mừng bạn đến với Discord",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: whiteColor,
+                      fontSize: 26,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    "Tham gia cùng hơn 100 triệu người dùng Discord và trò chuyện với bạn bè và các cộng đồng khác nhau.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: welcomeSecondaryColor,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(42),
+                padding: EdgeInsets.zero,
+                backgroundColor: welcomeRegisterButtonColor,
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3)),
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Đăng ký',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(42),
+                padding: EdgeInsets.zero,
+                backgroundColor: welcomeLoginButtonColor,
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3)),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SigninScreen()),
+                );
+              },
+              child: const Text(
+                'Đăng nhập',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
