06e7840c3b0af3ac15e4bb6b48f35a652366bed1
tunnvv <tunv.todo@gmail.com>
2022-10-29 21:53:22 +0700

    auto create REST API and add logic for servers schema

---
+++ backend_discord_clone/src/servers/dto/create-server.dto.ts
@@ -0,0 +1,7 @@
+export class CreateServerDto {
+    id: string;
+    name: string;
+    members: [];
+    chat_channels: [];
+    call_channels: [];
+}
---
+++ backend_discord_clone/src/servers/servers.service.ts
@@ -0,0 +1,45 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Server, ServerDocument } from 'src/schemas/servers.schema';
+import { CreateServerDto } from './dto/create-server.dto';
+import { UpdateServerDto } from './dto/update-server.dto';
+
+@Injectable()
+export class ServersService {
+
+  constructor(@InjectModel(Server.name) private serverModel: Model<ServerDocument>) {}
+
+  async create(createServerDto: CreateServerDto) {
+    const server = new this.serverModel(createServerDto);
+    return server.save();
+  }
+
+  async findAll() {
+    const servers = await this.serverModel.find().exec();
+    if (!servers || !servers[0]) {
+      throw new HttpException("Not Found", 404);
+    }
+    return servers;
+  }
+
+  async findOne(id: string) {
+    const server = await this.serverModel.findOne({id}).exec();
+    if (!server) {
+      throw new HttpException("Not Found", 404);
+    }
+    return server;
+  }
+
+  async update(id: string, updateServerDto: UpdateServerDto) {
+    return `This action updates a #${id} server`;
+  }
+
+  async remove(id: string) {
+    const server = await this.serverModel.deleteOne({id}).exec();
+    if (server.deletedCount === 0) {
+      throw new HttpException("Not Found", 404);
+    }
+    return server;
+  }
+}
