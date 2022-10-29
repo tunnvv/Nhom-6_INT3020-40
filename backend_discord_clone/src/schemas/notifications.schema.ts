fd3384df4abf892747c33387848794cc7421d426
tunnvv <tunv.todo@gmail.com>
2022-10-29 22:03:21 +0700

    closes #24; finished creating the basic backend

--- backend_discord_clone/src/schemas/notifications.schema.ts
+++ backend_discord_clone/src/schemas/notifications.schema.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import mongoose, { Document } from "mongoose";
 import { ChatChannel } from "./chat_channels.schema";
+import { Message } from "./messages.schema";
 import { User } from "./user.schema";
 
 export type NotificationDocument = Notification & Document;
