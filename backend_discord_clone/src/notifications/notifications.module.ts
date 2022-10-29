b4a6c8730613a71e23c4b4c3a04bbc94dbc9c737
tunnvv <tunv.todo@gmail.com>
2022-10-29 21:53:53 +0700

    auto create REST API and add logic for notifications schema

---
+++ backend_discord_clone/src/notifications/notifications.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { NotificationsService } from './notifications.service';
+import { NotificationsController } from './notifications.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Notification, NotificationSchema } from 'src/schemas/notifications.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: Notification.name, schema: NotificationSchema}])],
+  controllers: [NotificationsController],
+  providers: [NotificationsService]
+})
+export class NotificationsModule {}
