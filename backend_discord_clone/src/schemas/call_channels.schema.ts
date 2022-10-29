2f05545f53aa3dcfb877d9076685db278b9ac907
tunnvv <tunv.todo@gmail.com>
2022-10-29 21:40:28 +0700

    create my database schema use mongoose nestjs

---
+++ backend_discord_clone/src/schemas/call_channels.schema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { Document } from "mongoose";
+import { User } from "./user.schema";
+
+export type CallChannelDocument = CallChannel & Document;
+
+@Schema()
+export class CallChannel {
+    @Prop()
+    id: String;
+
+    @Prop()
+    name: String;
+
+    @Prop({ 
+        type: [{ type: mongoose.Schema.Types.ObjectId, ref: User.name }] 
+    })
+    members: User[];
+}
+export const CallChannelSchema = SchemaFactory.createForClass(CallChannel);
