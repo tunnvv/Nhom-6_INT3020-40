0901993f456fc948626fb10ddd78e8b7c1393a5b
tunnvv <tunv.todo@gmail.com>
2022-11-15 13:36:49 +0700

    completed basic message's functions

--- backend_discord_clone/src/messages/dto/create-message.dto.ts
+++ backend_discord_clone/src/messages/dto/create-message.dto.ts
@@ -1,8 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateMessageDto {
     _id: string;
+
+    @ApiProperty({required: true})
     user_id: string;
+
+    @ApiProperty({required: true})
     content: string;
+
+    @ApiProperty({required: true})
     create_at: string;
+
+    @ApiProperty({required: false})
     update_at: string;
+
+    @ApiProperty({required: false})
     reply: string;      // message_id
 }
--- backend_discord_clone/src/messages/messages.controller.ts
+++ backend_discord_clone/src/messages/messages.controller.ts
@@ -2,31 +2,41 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
+import { ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Message } from 'src/schemas/messages.schema';
 
+@ApiTags('messages')
 @Controller('messages')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
+  @ApiOkResponse({description: "Successfully created message"})
   @Post()
-  create(@Body() createMessageDto: CreateMessageDto) {
+  async create(@Body() createMessageDto: CreateMessageDto) {
     return this.messagesService.create(createMessageDto);
   }
 
+  @ApiOkResponse({isArray: true, description: "Successfully returned all messages"})
   @Get()
   findAll() {
     return this.messagesService.findAll();
   }
 
-  @Get('t/:id')
+  @ApiOkResponse({description: "Successfully finded message by Id"})
+  @ApiNotFoundResponse({description: "Can't find. The message's id doesn't exits"})
+  @Get(':id')
   findOne(@Param('id') id: string) {
     return this.messagesService.findOneByObjID(id);
   }
 
+  @ApiOkResponse({description: "Successfully updated message's content"})
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateMessageDto: UpdateMessageDto) {
     return this.messagesService.update(id, updateMessageDto);
   }
 
+  @ApiOkResponse({description: "Successfully deleted message"})
+  @ApiNotFoundResponse({description: "Can't find message to delete"})
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.messagesService.remove(id);
