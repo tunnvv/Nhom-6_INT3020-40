456d8af06eb13f93b411a836798736fde3dd4177
Bùi Văn Toán <buivantoanbg2001@gmail.com>
2022-12-01 17:32:12 +0700

    Update folder test, change img folder

---
+++ discord_clone/integration_test/widget_test.dart
@@ -0,0 +1,7 @@
+import '../test/widget_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  TestDiscordClone.run();
+}
